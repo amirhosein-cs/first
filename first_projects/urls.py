327a078921c2f8a17c1a1e718ee2b1c17f8d4561
amir <amir.heydari44445@gmail.com>
2026-07-27 23:31:46 +0330

    robots

--- first_projects/urls.py
+++ first_projects/urls.py
@@ -15,13 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+
 from django.urls import path, include
+
 from django.conf import settings
 from django.conf.urls.static import static
+
+from blog.sitemaps import BlogSitemap
+from first_app.sitemaps import StaticViewSitemap
+from blog.sitemaps import BlogSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap,
+    "blog": BlogSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('first_app.urls')),
     path('blog/',include('blog.urls')),
+    path("sitemap.xml",sitemap,{"sitemaps": sitemaps},  name="django.contrib.sitemaps.views.sitemap",
+    )
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
